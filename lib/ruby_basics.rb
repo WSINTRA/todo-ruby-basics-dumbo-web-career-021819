9ccf9aac58c6465bd51f38c09ffe9fdf510af670
William Robert Simpson <will.simpson85@gmail.com>
2019-01-03 15:32:48 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -6,16 +6,16 @@ def assign_variable(value)
  value
 end
 
-def argue
-
+def argue(inner)
+ inner
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
 def return_a_value
-
+nice = "Nice"
 end
 
 def last_evaluated_value
